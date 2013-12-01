a38dbacb34a2fa3d421bcb356f054fb8b4cb8c9c
Kevin McHugh <kev@kevinmchugh.me>
2013-11-30 23:28:25 -0600

    Added empty people route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Grouper::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :people
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
