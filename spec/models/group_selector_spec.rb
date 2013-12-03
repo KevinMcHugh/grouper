18f7ef612fbc45f7dc71de9c6320d47d2ef97d42
Kevin McHugh <kmchugh@backstopsolutions.com>
2013-12-02 21:05:23 -0600

    Eliminated a silly state bug

--- spec/models/group_selector_spec.rb
+++ spec/models/group_selector_spec.rb
@@ -2,15 +2,25 @@ require 'spec_helper'
 
 describe GroupSelector do
 	describe '.select' do
-		let (:alice) {Person.new({name: "alice", gender: :woman, start: Date.iso8601("2010-01-01")})}
+		let (:alice) {Person.new({name: "alice", gender: :woman, start: Date.iso8601("2009-01-01")})}
 		let (:bob) 	 {Person.new({name: "bob",   gender: :man,   start: Date.iso8601("2013-01-01")})}
 		let (:carol) {Person.new({name: "carol", gender: :woman, start: Date.iso8601("2010-01-01")})}
 		let (:dan) 	 {Person.new({name: "dan",   gender: :trans, start: Date.iso8601("2013-01-01")})}
 		
-		let (:result) {[[alice, bob],[carol, dan]]}
-		subject{GroupSelector.select [alice, bob, carol, dan]}
-		it 'selects the most diverse groups' do
-			expect(subject).to match_array result
+		context 'with an even number of people' do
+			let (:result) {[[alice, bob],[carol, dan]]}
+			subject{GroupSelector.select [alice, bob, carol, dan]}
+			it 'selects the most diverse groups' do
+				expect(subject).to match_array result
+			end
+		end
+
+		context 'with an odd number of people' do
+			let (:result) {[[alice, bob],[carol]]}
+			subject{GroupSelector.select [alice, bob, carol]}
+			it 'selects the most diverse groups' do
+				expect(subject).to match_array result
+			end
 		end
 	end
 end
