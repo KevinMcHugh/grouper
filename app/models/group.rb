2da0992be28a7995564a25e4b9b11a71fe66d1a8
Kevin McHugh <kmchugh@backstopsolutions.com>
2013-12-26 13:02:53 -0600

    Add == for Group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ActiveRecord::Base
 	belongs_to :event
 	has_and_belongs_to_many   :people
+
+	def == other
+		other.event == event && other.people == people
+	end
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Group do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let (:event) {Event.new}
+	let (:group_a) {Group.new({people: [], event: event})}
+	let (:group_b) {Group.new({people: [], event: nil})}
+
+	subject{Group.new({people: [], event: event})}
+	describe '==' do
+		context 'when objects are equal' do
+			it {should == group_a}
+		end
+
+		context 'when objects are not equal' do
+			it {should_not == group_b}
+		end
+	end
 end
