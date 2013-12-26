8bb1f5220e23f870be1a973b5e492c646a6fd76a
Kevin McHugh <kev@kevinmchugh.me>
2013-12-26 14:57:27 -0600

    Up group size to 4.
    Switched GroupSelector spec to use factory girl

--- spec/models/group_selector_spec.rb
+++ spec/models/group_selector_spec.rb
@@ -2,27 +2,33 @@ require 'spec_helper'
 
 describe GroupSelector do
 	describe '.select' do
-		let (:alice) {Person.new({name: "alice", gender: :woman, start: Date.iso8601("2009-01-01")})}
-		let (:bob) 	 {Person.new({name: "bob",   gender: :man,   start: Date.iso8601("2013-01-01")})}
-		let (:carol) {Person.new({name: "carol", gender: :woman, start: Date.iso8601("2010-01-01")})}
-		let (:dan) 	 {Person.new({name: "dan",   gender: :trans, start: Date.iso8601("2013-01-01")})}
-		let(:group1) {Group.new people: [alice, bob]}
+		let (:alice) {build(:person)}
+		let (:bob) 	 {build(:person, {name: "bob", gender: :man, start: "2013-01-01"})}
+		let (:carol) {build(:person, name: "carol")}
+		let (:dan) 	 {build(:person, {name: "dan", gender: :trans, start: "2013-01-01"})}
+		let (:e) 	 {build(:person, name: "e")}
+		let (:f) 	 {build(:person, {name: "f", gender: :man, start: "2013-01-01"})}
+		let (:g) 	 {build(:person, name: "g")}
+		let (:h) 	 {build(:person, {name: "h", gender: :trans, start: "2013-01-01"})}
+
 
 		context 'with an even number of people' do
-			let(:group2) {Group.new people: [carol, dan]}
+			let(:group1) {Group.new people: [alice, bob, carol, dan]}
+			let(:group2) {Group.new people: [e,f,g,h]}
 			let (:result) {[group1, group2]}
-			subject{GroupSelector.select [alice, bob, carol, dan]}
+			subject{GroupSelector.select [alice, bob, carol, dan, e,f,g,h]}
 			it 'selects the most diverse groups' do
 				expect(subject).to match_array result
 			end
 		end
 
 		context 'with an odd number of people' do
-			let(:group2) {Group.new people: [carol]}
-			let (:result) {[group1, group2]}
-			subject{GroupSelector.select [alice, bob, carol]}
+			let(:group1) {Group.new people: [dan, e,f,g]}
+			let(:group2) {Group.new people: [alice, bob, carol]}
+			let (:result) {[group1,group2]}
+			subject{GroupSelector.select [alice, bob, carol, dan, e,f,g]}
 			it 'selects the most diverse groups' do
-				expect(subject).to eq result
+				expect(subject).to match_array result
 			end
 		end
 	end
