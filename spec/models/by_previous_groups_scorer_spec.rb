416ce99a45b85020bc36b3e824cdd553d146d314
Kevin McHugh <kev@kevinmchugh.me>
2013-12-27 12:37:53 -0600

    fix weird indents.

--- spec/models/by_previous_groups_scorer_spec.rb
+++ spec/models/by_previous_groups_scorer_spec.rb
@@ -4,9 +4,9 @@ describe ByPreviousGroupsScorer do
 	describe '.score' do
 		context 'with valid arguments' do
 			let (:alice) {build(:person)}
-			let (:bob) 	 {build(:bob)}
+			let (:bob) {build(:bob)}
 			let (:carol) {build(:carol)}
-			let (:dan)   {build(:dan)}
+			let (:dan) {build(:dan)}
 			let (:group) {build(:group, people: [alice, bob])}
 
 			subject {ByPreviousGroupsScorer.score([[alice, bob],[alice, carol], [alice, dan],
@@ -18,11 +18,11 @@ describe ByPreviousGroupsScorer do
 
 			it 'scores to accentuate differences' do
 				expect(subject).to match_array [{group: [alice,bob],   score: 0},
-												{group: [alice,carol], score: 1},
-												{group: [alice,dan],   score: 1},
-												{group: [bob,carol],   score: 1},
-												{group: [bob, dan],    score: 1},
-												{group: [carol, dan],  score: 1}]
+				{group: [alice,carol], score: 1},
+				{group: [alice,dan],   score: 1},
+				{group: [bob,carol],   score: 1},
+				{group: [bob, dan],    score: 1},
+				{group: [carol, dan],  score: 1}]
 			end
   		end
   	end
