3bacff1c719b1eebb9b71b0edd9fa074c2f34598
Kevin McHugh <kmchugh@backstopsolutions.com>
2013-12-27 15:42:20 -0600

    Fix two bugs in ScoreTransformer.score_distances
    1. Equally scored distances did not work at all.
    2. the min score was always bumped up.

--- app/models/score_transformer.rb
+++ app/models/score_transformer.rb
@@ -7,11 +7,27 @@ class ScoreTransformer
     result
   end
   def self.score_distances group_distance_mapping
-      greatest_distance = group_distance_mapping.values.max
+      old_min = group_distance_mapping.values.min
+      old_max = group_distance_mapping.values.max
+      old_range = old_max - old_min
+      score_each_distance group_distance_mapping, old_min, old_max, old_range
+  end
+  private
+    def self.score_each_distance group_distance_mapping, old_min, old_max, old_range
       group_score_mapping = {}
+      return score_equally(group_distance_mapping) if old_range == 0
       group_distance_mapping.keys.each do |group|
-        group_score_mapping[group] = group_distance_mapping[group] / greatest_distance
+        group_score_mapping[group] = ((group_distance_mapping[group] - old_min) / old_range)
       end
       group_score_mapping
-  end
+    end
+
+    def self.score_equally group_distance_mapping
+      group_score_mapping = {}
+      group_distance_mapping.keys.each do |group|
+        group_score_mapping[group] = 1
+      end
+      group_score_mapping
+    end
+
 end
--- spec/models/score_transformer_spec.rb
+++ spec/models/score_transformer_spec.rb
@@ -1,14 +1,24 @@
 require 'spec_helper' 
 
 describe ScoreTransformer do
-  let(:group_scoring_mapping) {{['a'] => 1, ['b'] => 2}}
-  let(:result) do
-    [{group: ['a'], score: 1},{group: ['b'], score: 2}]
-  end
   describe '.transform' do
+    let(:group_scoring_mapping) {{['a'] => 1, ['b'] => 2}}
+    let(:result) do
+      [{group: ['a'], score: 1},{group: ['b'], score: 2}]
+    end
     subject{ScoreTransformer.transform group_scoring_mapping}
     it 'transforms properly' do
       expect(subject).to match_array result
+    end
+  end
+  describe '.score_distance' do
+    let(:group_scoring_mapping) {{['a'] => 1.5, ['b'] => 1.5}}
+    let(:result) do
+      {['a'] => 1, ['b'] => 1}
+    end
+    subject{ScoreTransformer.score_distances group_scoring_mapping}
+    it 'adjusts equal distance' do
+      expect(subject).to eq result
     end
   end
 end
