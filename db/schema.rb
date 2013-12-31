1e02963157982a13dcdd6a6e67651d822b74fb9b
Kevin McHugh <kev@kevinmchugh.me>
2013-12-30 23:23:43 -0600

    Add team to people.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131226173811) do
+ActiveRecord::Schema.define(version: 20131231051455) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20131226173811) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "group_id"
+    t.string   "team"
   end
 
 end
