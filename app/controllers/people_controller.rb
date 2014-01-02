0cbe034c811d63de129618c1e6b56af98f6411e8
Kevin McHugh <kev@kevinmchugh.me>
2014-01-02 00:08:54 -0600

    Made People Editable

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -6,7 +6,7 @@ class PeopleController < ApplicationController
   def create
     @person = Person.new(person_params)
     @person.save
-      redirect_to @person
+    redirect_to @person
   end
 
   def show
@@ -19,8 +19,21 @@ class PeopleController < ApplicationController
     redirect_to people_path
   end
 
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  def update
+    @person = Person.find(params[:id])
+    if @person.update(params[:person].permit(:name, :gender, :start, :team))
+      redirect_to @person
+    else
+      render 'edit'
+    end
+  end
+
   private
       def person_params
-          params.required(:person).permit(:name, :gender, :start)
+          params.required(:person).permit(:name, :gender, :start, :team)
       end
 end
