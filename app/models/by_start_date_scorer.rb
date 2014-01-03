61d8961cf6a418e7aa947370822ca8646bf55736
Kevin McHugh <kmchugh@backstopsolutions.com>
2014-01-03 15:30:47 -0600

    ByStartDateSelector supports >2 person groups

--- app/models/by_start_date_scorer.rb
+++ app/models/by_start_date_scorer.rb
@@ -1,5 +1,16 @@
 class ByStartDateScorer < ScorerLoop
   def self.score people_arrays 
-    score_loop(people_arrays) { |people|(people[0].start - people[1].start).abs }
+    score_loop(people_arrays) do |people|
+      score_people(people).to_f / people.length
+    end
   end
+
+  private
+    def self.score_people people
+      start_dates = people.map {|person| person.start }.sort
+      distances = (0..start_dates.length-2).map do |i|
+        start_dates[i+1] - start_dates[i]
+      end
+      distances.reduce :+
+    end
 end
--- spec/models/by_start_date_scorer_spec.rb
+++ spec/models/by_start_date_scorer_spec.rb
@@ -2,17 +2,29 @@ require 'spec_helper'
 
 describe ByStartDateScorer do
   describe '.score' do
-    context 'with valid arguments' do
-      let (:alice) {build(:person)}
-      let (:bob)   {build(:bob)}
-      let (:carol) {build(:carol)}
+    let (:alice) {build(:person)}
+    let (:bob)   {build(:bob)}
+    let (:carol) {build(:carol)}
+    let (:dan) {build(:dan)}
+    let (:e) {build(:person, name: 'e')}
+    let (:f) {build(:bob, name: 'f', start: "2008-01-01")}
 
+    context 'with 2 person groups' do
       subject {ByStartDateScorer.score([[alice, bob],[alice, carol], [bob, carol]])}
       it 'scores to accentuate differences' do
         expect(subject).to match_array [{group: [alice,bob],   score: 1},
                         {group: [alice,carol], score: 0},
                         {group: [bob,carol],   score: 1}]
       end
+    end
+    context 'with 4 person groups' do
+      subject {ByStartDateScorer.score([[alice,bob,carol,f], [alice,carol,dan,e],
+                                        [alice,bob,carol,dan]])}
+      it 'scores to accentuate differences' do
+        expect(subject).to match_array [{group: [alice,bob,carol,f], score: 1},
+                      {group: [alice,carol,dan,e], score: 0},
+                      {group: [alice,bob,carol,dan], score: 0.7496580027359782}]
       end
     end
+  end
 end
