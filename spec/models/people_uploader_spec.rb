d1c3de5e77aa0f6ef68612cd27a548380c871099
George Shakhnazaryan <gshakhnazaryan@backstopsolutions.com>
2014-01-22 15:53:01 -0600

    Rename person uploader to people uploader to match other stuff.

--- spec/models/people_uploader_spec.rb
+++ spec/models/people_uploader_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe PersonUploader do
+describe PeopleUploader do
   let(:alice) {build(:person)}
   let(:bob) {build(:bob)}
   let(:people) {[alice, bob]}
@@ -24,7 +24,7 @@ describe PersonUploader do
         person.stub(:save).and_return(true)
       end
     end
-    subject{PersonUploader.new.create_people(people)}
+    subject{PeopleUploader.new.create_people(people)}
     it "saves all the given People" do
       people.each do |person|
         expect(person).to receive(:save).once
@@ -34,19 +34,19 @@ describe PersonUploader do
   end
 
   context '#parse' do
-    subject {PersonUploader.new.parse yaml}
+    subject {PeopleUploader.new.parse yaml}
     it "parses the file" do
       expect(subject).to match_array people
     end 
   end
 
   context '#upload' do
-    let(:person_uploader) {PersonUploader.new}
+    let(:people_uploader) {PeopleUploader.new}
 
-    subject {person_uploader.upload yaml}
+    subject {people_uploader.upload yaml}
     it "parses and saves the data" do
-      person_uploader.should_receive(:parse).and_call_original
-      person_uploader.should_receive(:create_people).with(people).and_call_original
+      people_uploader.should_receive(:parse).and_call_original
+      people_uploader.should_receive(:create_people).with(people).and_call_original
       expect(subject).to match_array people
     end
   end
