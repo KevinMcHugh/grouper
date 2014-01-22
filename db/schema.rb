57f29a79475bbb9e4aba6335102e80bbcf9e659d
Kevin McHugh <kev@kevinmchugh.me>
2014-01-22 15:02:10 -0600

    Removed that weird group_id column from people

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131231051455) do
+ActiveRecord::Schema.define(version: 20140122195417) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 20131231051455) do
     t.date     "start"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "group_id"
     t.string   "team"
   end
 
