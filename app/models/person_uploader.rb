39f2ec911e280e7aedf13e961095a53bea78d00c
Kevin McHugh <kmchugh@backstopsolutions.com>
2014-01-22 11:02:22 -0600

    Add #parse to PersonUploader

--- app/models/person_uploader.rb
+++ app/models/person_uploader.rb
@@ -1,5 +1,14 @@
 class PersonUploader
 
+  def upload yaml
+    create_people parse(yaml)
+  end
+
+  def parse yaml
+    people = YAML.load(yaml)["people"]
+    people.map { |person| Person.new person}
+  end
+
   def create_people people
     people.each do |person|
       person.save
