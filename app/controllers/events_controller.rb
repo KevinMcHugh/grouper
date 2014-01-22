a24ac4765fa01635c70b5cf42463a74bf8a24cd5
Rob West <rwest@backstopsolutions.com>
2014-01-22 16:09:39 -0600

    Rework event and people ui * Pretty up the tables * Change single event display to display group members in a table * Change multi-event display to not display group members but just a number of groups

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,6 +17,12 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+    group_text = @event.groups.map.with_index(1) do |g, i|
+      ["Group #{i}"] + g.people.map {|p| p.name}
+    end
+    max_size = group_text.max {|r1, r2| r1.size <=> r2.size}.size
+    group_text.each {|r| r[max_size - 1] ||= nil }
+    @group_table = group_text.transpose
   end
 
   def destroy
