ad03035d8fd5a231cd71806e3bbe377924614f91
Kevin McHugh <kev@kevinmchugh.me>
2014-01-23 15:34:20 -0600

    Shorten a method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -56,13 +56,9 @@ class EventsController < ApplicationController
   def announce
     @event = Event.find(params[:id])
     went_last_time = @event.previous_event.people.to_a
-    went_last_time.map do |person|
-      EventMailer.opt_out_mail(@event, person).deliver
-    end
+    went_last_time.map {|person| EventMailer.opt_out_mail(@event, person).deliver }
     others = Person.all.to_a - went_last_time
-    others.map do |person|
-      EventMailer.opt_in_mail(@event, person).deliver
-    end
+    others.map { |person| EventMailer.opt_in_mail(@event, person).deliver }
     redirect_to email_sent_event_path
   end
 
