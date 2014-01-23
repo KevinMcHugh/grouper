117d0d832f6f3c4f5f4bf288a9f13912d019b107
Kevin McHugh <kmchugh@backstopsolutions.com>
2014-01-22 23:20:33 -0600

    EventsUploader should set the people on the event.

--- app/models/events_uploader.rb
+++ app/models/events_uploader.rb
@@ -6,13 +6,17 @@ class EventsUploader
   def parse yaml
     values = YAML.load(yaml)
     name = values["name"]
+    all_people = []
     groups = values["groups"].map do |group|
       people = group.map do |name|
         Person.find_by name: name
       end
+      all_people << people
       Group.new people: people
     end
-    Event.new(groups: groups, name: name)
+    event = Event.new(groups: groups, name: name)
+    all_people.map {|p| event.add_person p}
+    event
   end
 
   def create_event event
--- spec/models/events_uploader_spec.rb
+++ spec/models/events_uploader_spec.rb
@@ -12,15 +12,20 @@ describe EventsUploader do
      - dan"
    end
   let(:event) {build(:event)}
+  let(:event_uploader) {EventsUploader.new}
+  let(:alice) {build(:person)}
+  let(:bob) {build(:bob)}
+  let(:carol) {build(:carol)}
+  let(:dan) {build(:dan)}
 
   context "#upload" do
-    let(:event_uploader) {EventsUploader.new}
 
     subject {event_uploader.upload yaml}
     it "parses and saves the data" do
       event_uploader.should_receive(:parse).and_call_original
       event_uploader.should_receive(:create_event).with(event).and_call_original
       expect(subject).to eq event
+      expect(subject.people.map(&:name)).to match_array [alice,bob,carol,dan].map(&:name)
     end
   end 
 end
