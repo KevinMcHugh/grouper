4cae621293a16781ac8b362b9b89a75bc9ceca0a
Kevin McHugh <kev@kevinmchugh.me>
2014-01-23 10:37:38 -0600

    Do a better thing with showing events

--- app/controllers/events_uploaders_controller.rb
+++ app/controllers/events_uploaders_controller.rb
@@ -3,6 +3,7 @@ class EventsUploadersController < ApplicationController
     events_uploader = EventsUploader.new
     file = params["events_uploader"]["file"].read
     @event = events_uploader.upload file
-    render 'events/show'
+    @people = @event.people
+    redirect_to event_path(@event)
   end
 end
