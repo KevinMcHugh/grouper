0eefd875f3aa62f2ac5b4734200e83771644a2c3
Kevin McHugh <kev@kevinmchugh.me>
2014-01-23 15:32:07 -0600

    Better name for group_mail, no more looping in mailer

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,9 +1,7 @@
 class EventMailer < ActionMailer::Base
-  def event_mail event
-    event.groups.each do |group|
-      email_addresses = group.people.map(&:email_address)
-      mail(to: email_addresses, subject: event.name).deliver
-    end
+  def group_mail group, event_name
+    email_addresses = group.people.map(&:email_address)
+    mail(to: email_addresses, subject: event_name)
   end
 
   def opt_in_mail event, person
