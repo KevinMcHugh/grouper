ff5d299154e0e65566e849cb936c367e7e437b19
Kevin McHugh <kmchugh@backstopsolutions.com>
2014-01-24 11:12:59 -0600

    Shorter methods in EventsUploader

--- app/models/events_uploader.rb
+++ app/models/events_uploader.rb
@@ -6,17 +6,26 @@ class EventsUploader
   def parse yaml
     values = YAML.load(yaml)
     name = values["name"]
-    all_people = []
-    groups = values["groups"].map do |group|
-      people = group.map do |name|
-        Person.find_by name: name
-      end
-      all_people << people
-      Group.new people: people
-    end
+    groups = groups_from values["groups"]
     event = Event.new(groups: groups, name: name)
-    all_people.map {|p| event.add_person p}
+    add_people_in_groups_to_event groups, event 
     event
+  end
+
+  def groups_from groups
+    groups.map do |people_names|
+      Group.new people: people_in(people_names)
+    end
+  end
+
+  def add_people_in_groups_to_event groups, event
+    groups.each do |group|
+      group.people.each {|p| event.add_person p}
+    end
+  end
+
+  def people_in people_names
+    people_names.map {|name| Person.find_by name: name }
   end
 
   def create_event event
