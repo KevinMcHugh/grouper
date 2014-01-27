e50748fdd878320ca97a29d85ad2df6efe628db0
Kevin McHugh <kev@kevinmchugh.me>
2014-01-26 20:37:11 -0600

    Best practices, use locals instead of instance

--- app/controllers/events_uploaders_controller.rb
+++ app/controllers/events_uploaders_controller.rb
@@ -3,7 +3,6 @@ class EventsUploadersController < ApplicationController
     events_uploader = EventsUploader.new
     file = params["events_uploader"]["file"].read
     @event = events_uploader.upload file
-    @people = @event.people
     redirect_to event_path(@event)
   end
 end
