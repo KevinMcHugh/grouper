9185de5e00d6566d9454da2d79b137f314fa0164
Kevin McHugh <kmchugh@backstopsolutions.com>
2014-01-26 19:13:28 -0600

    Remove unused methods

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,6 @@
 class Person < ActiveRecord::Base
 
   has_and_belongs_to_many :groups
-  def man?; gender.to_sym.eql? :man; end
-  def woman?; gender.to_sym.eql? :woman; end
   def inspect; "#{name}:#{id}"; end
   def to_s; name; end
 
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -8,50 +8,7 @@ describe Person do
   describe '#new' do
     subject {bob}
     it 'should symbolize the argument' do
-      expect(subject.man?).to be_true
-      expect(subject.woman?).to be_false
-    end
-  end
-
-  describe '#man?' do
-    context 'when person is a woman' do
-      subject {alice}
-      it 'returns false' do
-        expect(subject.man?).to be_false
-      end
-    end
-    context 'when person is trans' do
-      subject {dan}
-      it 'returns false' do
-        expect(subject.man?).to be_false
-      end
-    end
-    context 'when person is a man' do
-      subject {bob}
-      it 'returns true' do
-        expect(subject.man?).to be_true
-      end
-    end
-  end
-
-  describe '#woman?' do
-    context 'when person is a woman' do
-      subject {alice}
-      it 'returns true' do
-        expect(subject.woman?).to be_true
-      end
-    end
-    context 'when person is trans' do
-      subject {dan}
-      it 'returns false' do
-        expect(subject.woman?).to be_false
-      end
-    end
-    context 'when person is a man' do
-      subject {bob}
-      it 'returns true' do
-        expect(subject.woman?).to be_false
-      end
+      expect(subject.gender).to eq :man
     end
   end
 
