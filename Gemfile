03914f2999629bc9e6be248e6890e83914615793
Kevin McHugh <kev@kevinmchugh.me>
2015-01-19 22:21:07 -0600

    Adds mandrill

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 gem 'haml-rails', '~> 0.3.4'
+gem 'mandrill-api'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,8 @@ Grouper::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.default_url_options = { host: 'radiant-river-6971.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 end
